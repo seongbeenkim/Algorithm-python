4c4b07103087aff79ef8e5f8e952a0d05c22ed06
Beenie93 <seongbeen93@naver.com>
2020-01-26 18:31:44 +0900

    https://www.acmicpc.net/problem/11726

---
+++ BOJ(Baekjoon Online Judge)/DP/11726_2xn 타일링(2xn tailing).py
@@ -0,0 +1,32 @@
+#https://www.acmicpc.net/problem/11726
+
+import sys
+sys.setrecursionlimit(10**6)
+
+# Bottom-up
+
+n = int(sys.stdin.readline())
+d = [0] * (n+1)
+d[0] = d[1] = 1
+
+for i in range(2,n+1):
+    d[i] = (d[i-1] + d[i-2])%10007
+print(d[n])
+
+# Top-down
+"""
+def dp(n):
+    if n <= 1:
+        return 1
+    if d[n] > 0:
+        return d[n]
+
+    d[n] = (dp(n-1) + dp(n-2))%10007
+    return d[n]
+
+
+n = int(sys.stdin.readline())
+d = [0] * (n+1)
+d[0] = d[1] = 1
+print(dp(n))
+"""
