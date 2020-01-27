f8790fe2ddab3a43c36bde7a2cd4fc3c198fca44
Beenie93 <seongbeen93@naver.com>
2020-01-28 00:45:31 +0900

    https://www.acmicpc.net/problem/10844

---
+++ BOJ(Baekjoon Online Judge)/DP/10844_쉬운 계단 수(easy staircase number).py
@@ -0,0 +1,22 @@
+#https://www.acmicpc.net/problem/10844
+import sys
+
+max = 101
+d = [[0] * 10 for i in range(max)]
+mod = 1000000000
+for i in range(1,10):
+    d[1][i] = 1
+
+for i in range(2,max):
+    for j in range(10):
+        if 1 <= j <= 8:
+            d[i][j] = d[i-1][j-1] + d[i-1][j+1]
+        elif j == 9:
+            d[i][j] = d[i-1][j-1]
+        elif j == 0:
+            d[i][j] = d[i-1][j+1]
+        d[i][j] %= mod
+
+n = int(sys.stdin.readline())
+print(sum(d[n])%mod)
+
