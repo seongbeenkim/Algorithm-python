255b0422df3540c02d0d8ec63af6708a9536bc1a
Beenie93 <seongbeen93@naver.com>
2020-01-27 23:37:21 +0900

    https://www.acmicpc.net/problem/15990

---
+++ BOJ(Baekjoon Online Judge)/DP/15990_1,2,3 더하기 2 (adding 1,2,3 2).py
@@ -0,0 +1,30 @@
+#https://www.acmicpc.net/problem/15990
+
+import sys
+
+max = 100001
+mod = 1000000009
+d = [[0] * 4 for i in range(max)]
+d[0][1] = d[0][2] = d[0][3] = 1
+for i in range(1, max):
+    if i - 1 >= 0:
+        d[i][1] = d[i - 1][2] + d[i - 1][3]
+        if i == 1:
+            d[i][1] = 1
+    if i - 2 >= 0:
+        d[i][2] = d[i - 2][1] + d[i - 2][3]
+        if i == 2:
+            d[i][2] = 1
+    if i - 3 >= 0:
+        d[i][3] = d[i - 3][1] + d[i - 3][2]
+        if i == 3:
+            d[i][3] = 1
+
+    d[i][1] %= mod
+    d[i][2] %= mod
+    d[i][3] %= mod
+
+T = int(sys.stdin.readline())
+for i in range(T):
+    n = int(sys.stdin.readline())
+    print(sum(d[n]) % mod)
