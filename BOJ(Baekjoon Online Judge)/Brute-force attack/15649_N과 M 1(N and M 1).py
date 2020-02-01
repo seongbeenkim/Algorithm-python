2bfcd62dc71f6fbbd2fae82e910222a8b93b9e76
Beenie93 <seongbeen93@naver.com>
2020-02-01 22:06:37 +0900

    https://www.acmicpc.net/problem/15649

---
+++ BOJ(Baekjoon Online Judge)/Brute-force attack/15649_N과 M 1(N and M 1).py
@@ -0,0 +1,21 @@
+#https://www.acmicpc.net/problem/15649
+import sys
+
+n, m = map(int,sys.stdin.readline().split())
+c = [False]* (n+1)
+a = [0] * m
+
+def go(index,n,m):
+    if index == m:
+        sys.stdout.write(" ".join(map(str,a)) + "\n" )
+        return
+
+    for i in range(1,n+1):
+        if c[i] == True:
+            continue
+        c[i] = True
+        a[index] = i
+        go(index+1,n,m)
+        c[i] = False
+
+go(0,n,m)
