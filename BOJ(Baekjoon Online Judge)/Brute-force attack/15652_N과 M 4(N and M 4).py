51c48bc1e0d4c5d6b3f0c6799ea762f71786f80c
Beenie93 <seongbeen93@naver.com>
2020-02-01 22:56:19 +0900

    https://www.acmicpc.net/problem/15651

--- BOJ(Baekjoon Online Judge)/Brute-force attack/15652_N과 M 4(N and M 4).py
+++ BOJ(Baekjoon Online Judge)/Brute-force attack/15652_N과 M 4(N and M 4).py
@@ -1,15 +1,15 @@
-#https://www.acmicpc.net/problem/15651
+#https://www.acmicpc.net/problem/15652
 import sys
 
 sys.setrecursionlimit(10**9)
 n, m = map(int,sys.stdin.readline().split())
 ans = [0] * m
 
-def go(index,n,m):
+def go(index,start,n,m):
     if index == m:
         sys.stdout.write(" ".join(map(str,ans)) + "\n")
         return
-    for i in range(1,n+1):
+    for i in range(start,n+1):
         ans[index] = i
-        go(index+1,n,m)
-go(0,n,m)
+        go(index+1,ans[index],n,m)
+go(0,1,n,m)
