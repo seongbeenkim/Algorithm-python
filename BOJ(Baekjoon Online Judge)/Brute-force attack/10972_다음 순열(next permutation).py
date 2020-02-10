ab2e8f611084de53c0e62b2a8907daaa26e51bd5
Beenie93 <seongbeen93@naver.com>
2020-02-10 17:15:03 +0900

    `https://www.acmicpc.net/problem/10972

---
+++ BOJ(Baekjoon Online Judge)/Brute-force attack/10972_다음 순열(next permutation).py
@@ -0,0 +1,34 @@
+#https://www.acmicpc.net/problem/10972
+
+import sys
+
+n = int(sys.stdin.readline())
+a = list(map(int,sys.stdin.readline().split()))
+max = [0] * n
+def go(a):
+    i = n - 1
+
+    while i > 0 and a[i] < a[i-1]:
+        i -= 1
+    if i <= 0:
+        return False
+
+    j = n - 1
+
+    while a[j] <= a[i-1]:
+        j -= 1
+
+    a[j], a[i-1] = a[i-1], a[j]
+
+    j = n - 1
+    while i < j:
+        a[i], a[j] = a[j], a[i]
+        i+=1
+        j-=1
+
+    return True
+
+if go(a):
+    print(" ".join(map(str,a)))
+else:
+    print(-1)
