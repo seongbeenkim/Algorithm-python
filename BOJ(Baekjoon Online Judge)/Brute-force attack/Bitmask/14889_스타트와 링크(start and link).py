3b4cbed028d616c46a160dbe1a882320f08d257f
Beenie93 <seongbeen93@naver.com>
2020-02-14 00:51:21 +0900

    `https://www.acmicpc.net/problem/14889

--- BOJ(Baekjoon Online Judge)/Brute-force attack/Bitmask/14889_스타트와 링크(start and link).py
+++ BOJ(Baekjoon Online Judge)/Brute-force attack/Bitmask/14889_스타트와 링크(start and link).py
@@ -5,7 +5,7 @@ import sys
 n = int(sys.stdin.readline())
 s = [list(map(int,sys.stdin.readline().split())) for i in range(n)]
 ans = 2147483647
-for i in range(1 << ((n//2)-1) ,1<<n):
+for i in range(1 << ((n//2)-1), 1<<n):
     start = []
     link = []
     for j in range(n):
@@ -13,20 +13,20 @@ for i in range(1 << ((n//2)-1) ,1<<n):
             start += [j]
         else:
             link += [j]
+    if len(start) != n//2:
+        continue
 
-    if len(start) == len(link):
-        start_sum = 0
-        link_sum = 0
+    start_sum = 0
+    link_sum = 0
 
-        for k in range(n//2):
-            for l in range(k+1,n//2):
-                if k == (n/2)-1 and l == (n//2)-1:
-                    continue
-                start_sum += s[start[k]][start[l]] + s[start[l]][start[k]]
+    for k in range((n//2)-1):
+        for l in range(k+1,n//2):
 
-                link_sum += s[link[k]][link[l]] + s[link[l]][link[k]]
+            start_sum += s[start[k]][start[l]] + s[start[l]][start[k]]
 
-                res = abs(start_sum - link_sum)
-                if ans > res:
-                    ans = res
-print(res)
+            link_sum += s[link[k]][link[l]] + s[link[l]][link[k]]
+
+    res = abs(start_sum - link_sum)
+    if ans > res:
+        ans = res
+print(ans)
