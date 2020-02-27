81f829923300424e6d02034ac0ae3dfcf5c9a834
Beenie93 <seongbeen93@naver.com>
2020-02-27 23:17:22 +0900

    https://www.acmicpc.net/problem/11725

--- BOJ(Baekjoon Online Judge)/Graph/11725_트리의 부모 찾기(find the parent of a tree).py
+++ BOJ(Baekjoon Online Judge)/Graph/11725_트리의 부모 찾기(find the parent of a tree).py
@@ -2,29 +2,36 @@
 
 import sys
 from collections import deque
-
+sys.setrecursionlimit(10**6)
 n = int(sys.stdin.readline())
 d = [[] for _ in range(n+1)]
 parent = [0] * (n+1)
 check = [False] * (n+1)
-depth = [0] * (n+1)
+depth = [1] * (n+1)
 q = deque()
 
 for _ in range(n-1):
     u, v = map(int,sys.stdin.readline().split())
     d[u].append(v)
     d[v].append(u)
-
-q.append(1)
-check[1] = True
-depth[1] = 1
-while q:
-    x = q.popleft()
-    for i in d[x]:
+def bfs(start):
+    q.append(start)
+    check[start] = True
+    while q:
+        x = q.popleft()
+        for i in d[x]:
+            if check[i] == False:
+                q.append(i)
+                check[i] = True
+                depth[i] = depth[x] + 1; parent[i] = x
+def dfs(start):
+    check[start] = True
+    for i in d[start]:
         if check[i] == False:
-            q.append(i)
             check[i] = True
-            depth[i] = depth[x] + 1
-            parent[i] = x
+            depth[i] = depth[start] + 1
+            parent[i] = start
+            dfs(i)
+bfs(1) # dfs(1)
 for i in range(2,n+1):
     print(parent[i])
