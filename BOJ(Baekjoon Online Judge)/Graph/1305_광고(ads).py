5bc8641acf480744c9c8acace0b32b2aaa3212ce
Beenie93 <seongbeen93@naver.com>
2020-03-06 00:38:49 +0900

    https://www.acmicpc.net/problem/1305

---
+++ BOJ(Baekjoon Online Judge)/Graph/1305_광고(ads).py
@@ -0,0 +1,21 @@
+#https://www.acmicpc.net/problem/1305
+
+import sys
+
+l = int(sys.stdin.readline())
+t = sys.stdin.readline().strip()
+n = len(t)
+fail = [0] * n
+def pre_processing():
+    j = 0
+    for i in range(1,n):
+        while j > 0 and t[i] != t[j]:
+            j =fail[j-1]
+        if t[i] == t[j]:
+            fail[i] = j+1
+            j += 1
+        else:
+            j = 0
+
+pre_processing()
+print(l-fail[l-1])
