4c507741216d32a01b02be939130d6e4b270cd34
Beenie93 <seongbeen93@naver.com>
2020-03-11 01:46:30 +0900

    https://www.acmicpc.net/problem/1285

---
+++ BOJ(Baekjoon Online Judge)/Greedy/1285_동전 뒤집기(flipping coins).py
@@ -0,0 +1,23 @@
+#https://www.acmicpc.net/problem/1285
+
+import sys
+
+n = int(sys.stdin.readline())
+a = [list(sys.stdin.readline().rstrip()) for _ in range(n)]
+ans = 401
+for k in range(1<<n):
+    sum = 0
+    for j in range(n):
+        cnt = 0
+        for i in range(n):
+            c = a[i][j]
+            if c == "T":
+                c = "H"
+            else:
+                c = "T"
+            if c == "T":
+                cnt += 1
+        sum += min(n-cnt,cnt)
+    if ans > sum:
+        ans = sum
+print(ans)
