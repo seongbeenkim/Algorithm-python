08d562f5242fa745369451c03f376e118aac9ab5
Beenie93 <seongbeen93@naver.com>
2020-03-23 03:09:54 +0900

    https://www.acmicpc.net/problem/10816

---
+++ BOJ(Baekjoon Online Judge)/Divide & Conquer/10816_숫자 카드 2(number card 2).py
@@ -0,0 +1,73 @@
+#https://www.acmicpc.net/problem/10815
+
+import sys, bisect
+from collections import Counter
+
+n = int(sys.stdin.readline())
+card = list(map(int,sys.stdin.readline().split()))
+m = int(sys.stdin.readline())
+numbers = list(map(int,sys.stdin.readline().split()))
+
+card.sort()
+### solve 1
+"""
+def lower_bound(num):
+    left = 0
+    right = len(card)-1
+    mid = 0
+    ans = -1
+    while left <= right:
+        mid = (mid + right) // 2
+        if card[mid] == num:
+            ans = mid
+            right = mid - 1
+        elif card[mid] < num:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return ans
+
+def upper_bound(num):
+    left = 0
+    right = len(card) - 1
+    mid = 0
+    ans = -1
+    while left <= right:
+        mid = (left+right) // 2
+        if card[mid] == num:
+            ans = mid + 1
+            left = mid+1
+        elif card[mid] < num:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return ans
+
+for x in numbers:
+    l = lower_bound(x)
+    u = upper_bound(x)
+    if l == -1:
+        print(0, end= " ")
+    else:
+        print(u-l, end = " ")
+"""
+
+### solve 2
+"""
+counter = Counter(card)
+res = [0] * m
+for x in numbers:
+    if x in counter:
+        print(counter[x] , end = " ")
+    else:
+        print(0, end = " ") 
+"""
+
+### solve 3
+for i in range(m):
+    idx = bisect.bisect_left(card,numbers[i])
+    if idx < len(card) and card[idx] == numbers[i]:
+        idx2 = bisect.bisect(card,numbers[i])
+        print(idx2-idx, end = " ")
+    else:
+        print(0, end = " ")
