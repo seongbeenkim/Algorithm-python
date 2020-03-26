909632459e5d277212e0ea33bbb865fde28d7b66
Beenie93 <seongbeen93@naver.com>
2020-03-26 23:53:58 +0900

    https://www.acmicpc.net/problem/1337

---
+++ BOJ(Baekjoon Online Judge)/Divide & Conquer/1337_버블 소트(bubble sort).py
@@ -0,0 +1,16 @@
+#https://www.acmicpc.net/problem/1377
+
+import sys
+read = lambda : sys.stdin.readline().strip()
+write = lambda x: sys.stdout.write(str(x)+ "\n")
+
+a = []
+n = int(read())
+for i in range(n):
+    a.append((int(read()), i))
+a.sort()
+
+ans = []
+for i in range(n):
+    ans.append(a[i][1] - i)
+write(max(ans)+1)
