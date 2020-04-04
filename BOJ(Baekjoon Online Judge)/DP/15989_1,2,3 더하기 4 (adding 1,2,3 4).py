f96436d11442477ae6e9768350704b798081512e
Beenie93 <seongbeen93@naver.com>
2020-04-05 00:54:48 +0900

    https://www.acmicpc.net/problem/15989

--- BOJ(Baekjoon Online Judge)/DP/15989_1,2,3 더하기 4 (adding 1,2,3 4).py
+++ BOJ(Baekjoon Online Judge)/DP/15989_1,2,3 더하기 4 (adding 1,2,3 4).py
@@ -6,14 +6,11 @@ n = int(sys.stdin.readline())
 d = [0] * 10001
 d[0] = 1
 for i in range(1,10001):
-    if i-1>=0:
-        d[i] += d[i-1]
+    d[i] += d[i-1]
 for i in range(2,10001):
-    if i-2>=0:
-        d[i] += d[i-2]
+    d[i] += d[i-2]
 for i in range(3,10001):
-    if i-3>=0:
-        d[i] += d[i-3]
+    d[i] += d[i-3]
 for i in range(n):
     a = int(sys.stdin.readline())
     print(d[a])
