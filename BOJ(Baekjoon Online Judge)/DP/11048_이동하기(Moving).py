bfd5f543a682a30b17438b24286e7b9520542180
Beenie93 <seongbeen93@naver.com>
2020-04-04 10:37:13 +0900

    https://www.acmicpc.net/problem/11048

--- BOJ(Baekjoon Online Judge)/DP/11048_이동하기(Moving).py
+++ BOJ(Baekjoon Online Judge)/DP/11048_이동하기(Moving).py
@@ -3,10 +3,32 @@
 import sys
 
 n, m = map(int,sys.stdin.readline().split())
-a = [list(map(int,sys.stdin.readline().split())) for i in range(n)]
-d = [[0] * (m+1) for i in range(n+1)]
+a = [[0] * (m+1)] + [[0] + list(map(int,sys.stdin.readline().split())) for i in range(n)]
+d = [[0] * (m+1) for _ in range(n+1)]
 
 for i in range(1,n+1):
     for j in range(1,m+1):
-        d[i][j] = max(d[i-1][j],d[i][j-1],d[i-1][j-1]) + a[i-1][j-1]
+        d[i][j] = max(d[i-1][j],d[i][j-1]) + a[i][j]
 print(d[n][m])
+
+"""
+def dp(x,y):
+    if x>n or y>m:
+        return 0
+    if d[x][y] >= 0:
+        return d[x][y]
+    d[x][y] = max(dp(x+1,y),dp(x,y+1)) + a[x][y]
+    return d[x][y]
+print(dp(1,1))
+"""
+
+"""
+d[1][1] = a[1][1]
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        if i+1 <= n:
+            d[i+1][j] = max(d[i+1][j],d[i][j] + a[i+1][j])
+        if j+1 <= m:
+            d[i][j+1] = max(d[i][j+1],d[i][j] + a[i][j+1])
+print(d[n][m])
+"""
