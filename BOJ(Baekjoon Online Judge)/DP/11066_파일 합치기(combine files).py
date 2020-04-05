2d28021586af4f507dbc40afe55b928bc8dbd850
Beenie93 <seongbeen93@naver.com>
2020-04-05 15:43:10 +0900

    https://www.acmicpc.net/problem/11066

---
+++ BOJ(Baekjoon Online Judge)/DP/11066_파일 합치기(combine files).py
@@ -0,0 +1,25 @@
+#https://www.acmicpc.net/problem/11066
+
+import sys
+sys.setrecursionlimit(10**6)
+t = int(sys.stdin.readline())
+
+def go(x,y):
+    if x == y:
+        return 0
+    if d[x][y] != -1:
+        return d[x][y]
+    ans = d[x][y]
+    cost = sum(a[x:y+1])
+    for k in range(x,y):
+        temp = go(x,k) + go(k+1,y) + cost
+        if ans == -1 or ans > temp:
+            ans = temp
+    d[x][y] = ans
+    return ans
+
+for _ in range(t):
+    n = int(sys.stdin.readline())
+    a = [0] + list(map(int,sys.stdin.readline().split()))
+    d = [[-1] * (n+1) for _ in range(n+1)]
+    print(go(1,n))
