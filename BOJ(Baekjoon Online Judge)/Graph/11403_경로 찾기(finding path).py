a2094e4e62f5b72cbaa4484449d29d262199fae7
Beenie93 <seongbeen93@naver.com>
2020-04-18 21:41:09 +0900

    https://www.acmicpc.net/problem/11403

---
+++ BOJ(Baekjoon Online Judge)/Graph/11403_경로 찾기(finding path).py
@@ -0,0 +1,15 @@
+#https://www.acmicpc.net/problem/11403
+
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+d = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if d[i][k] != 0 and d[k][j] != 0:
+                d[i][j] = 1
+for i in range(n):
+    print(*d[i])
