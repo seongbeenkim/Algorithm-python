d1b36ce8cbe993e0c0ca9a88dea5f294f775dfe8
Beenie93 <seongbeen93@naver.com>
2020-04-26 19:51:36 +0900

    https://www.acmicpc.net/problem/1182

--- BOJ(Baekjoon Online Judge)/Brute-force attack/Bitmask/1182_부분수열의 합(sum of subsequence).py
+++ BOJ(Baekjoon Online Judge)/Brute-force attack/Bitmask/1182_부분수열의 합(sum of subsequence).py
@@ -14,3 +14,22 @@ for i in range(1, 1<<n):
        cnt += 1
 
 print(cnt)
+
+"""
+import sys
+
+def go(index,sum):
+
+    global cnt
+    if index == n:
+        if sum == s:
+            cnt += 1
+        return
+    go(index+1,sum+k[index])
+    go(index+1,sum)
+
+go(0,0)
+if s == 0:
+    cnt -= 1
+print(cnt)
+"""
