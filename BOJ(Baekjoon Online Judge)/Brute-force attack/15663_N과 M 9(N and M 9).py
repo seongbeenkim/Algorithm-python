a11ba4d707e02b56f93a300e7ace7cf2194dd268
Beenie93 <seongbeen93@naver.com>
2020-04-27 23:39:26 +0900

    https://www.acmicpc.net/problem/15663

---
+++ BOJ(Baekjoon Online Judge)/Brute-force attack/15663_N과 M 9(N and M 9).py
@@ -0,0 +1,49 @@
+#https://www.acmicpc.net/problem/15663
+
+import sys
+from collections import Counter
+"""
+n,m = map(int,sys.stdin.readline().split())
+a = list(map(int,sys.stdin.readline().split()))
+a.sort()
+
+ans = []
+check = [False] * (n+1)
+
+def go(index,seq):
+    if index == m and len(seq) == m:
+        ans.append(tuple(seq))
+        return
+    if index >= len(a):
+        return
+
+    for i in range(len(a)):
+        if check[i]:
+            continue
+        check[i] = True
+        go(index+1,seq+[a[i]])
+        check[i] = False
+go(0,[])
+d = list(set(ans))
+d.sort()
+for x in d:
+    print(*x)
+"""
+
+n,m = map(int,sys.stdin.readline().split())
+a = list(map(int,sys.stdin.readline().split()))
+a = list(Counter(a).items())
+a.sort()
+
+num, check = map(list,zip(*a))
+n = len(num)
+def go(index,seq):
+    if index == m and len(seq) == m:
+        print(*seq)
+        return
+    for i in range(n):
+        if check[i] > 0:
+            check[i] -= 1
+            go(index+1,seq+[num[i]])
+            check[i] += 1
+go(0,[])
