c0b75db2efc11ad8face6a18c821155f77ee3925
Beenie93 <seongbeen93@naver.com>
2020-04-28 21:54:49 +0900

    https://www.acmicpc.net/problem/11724

--- BOJ(Baekjoon Online Judge)/Graph/11724_연결 요소(connected component).py
+++ BOJ(Baekjoon Online Judge)/Graph/11724_연결 요소(connected component).py
@@ -1,18 +1,18 @@
 #https://www.acmicpc.net/problem/11724
 
-import sys, queue
+import sys
+from collections import deque
+
 sys.setrecursionlimit(10**6)
+
 n, m = map(int,sys.stdin.readline().split())
 a = [[] for _ in range(n+1)]
 check = [False] * (n+1)
-ans = 0
+
 for i in range(m):
     u,v = map(int,sys.stdin.readline().split())
     a[u].append(v)
     a[v].append(u)
-
-for i in range(1,n+1):
-    a[i].sort()
 
 def dfs(x):
     check[x] = True
@@ -21,16 +21,17 @@ def dfs(x):
             dfs(i)
 
 def bfs(i):
-    cnt = 0
-    q = queue.Queue()
-    q.put(i)
+    q = deque()
+    q.append(i)
     check[i] = True
-    while not q.empty():
-        x = q.get()
+    while q:
+        x = q.popleft()
         for i in a[x]:
             if check[i] == False:
                 check[i] = True
-                q.put(i)
+                q.append(i)
+
+ans = 0
 for i in range(1,n+1):
     if not check[i]:
         bfs(i) #dfs(i)
