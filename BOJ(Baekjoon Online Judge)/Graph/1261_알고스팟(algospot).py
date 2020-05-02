b17938f257fd7c2e3e1aa30e1b75084a1ff35ab8
Beenie93 <seongbeen93@naver.com>
2020-05-02 16:17:20 +0900

    https://www.acmicpc.net/problem/1261

--- BOJ(Baekjoon Online Judge)/Graph/1261_알고스팟(algospot).py
+++ BOJ(Baekjoon Online Judge)/Graph/1261_알고스팟(algospot).py
@@ -6,7 +6,7 @@ from collections import deque
 m, n = map(int,sys.stdin.readline().split())
 a = [list(map(int,sys.stdin.readline().strip())) for i in range(n)]
 d = [[-1]*m for i in range(n)]
-check = [[False]*m for _ in range(n)]
+
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
@@ -14,21 +14,18 @@ def bfs(start,end):
     q = deque()
     q.append((start,end))
     d[start][end] = 0
-    check[start][end] = True
 
     while q:
         x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and check[nx][ny] == False:
+            if 0 <= nx < n and 0 <= ny < m and d[nx][ny] == -1:
                 if a[nx][ny] == 1:
                     q.append((nx, ny))
-                    check[nx][ny] = True
                     d[nx][ny] = d[x][y] + 1
                 else:
                     q.appendleft((nx, ny))
-                    check[nx][ny] = True
                     d[nx][ny] = d[x][y]
 bfs(0,0)
 print(d[n-1][m-1])
