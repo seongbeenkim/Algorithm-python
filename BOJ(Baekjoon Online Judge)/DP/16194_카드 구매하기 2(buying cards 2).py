f31433fc33f07f12b6835fbf5e77278de15d4296
Beenie93 <seongbeen93@naver.com>
2020-05-04 03:08:42 +0900

    https://www.acmicpc.net/problem/16194

--- BOJ(Baekjoon Online Judge)/DP/16194_카드 구매하기 2(buying cards 2).py
+++ BOJ(Baekjoon Online Judge)/DP/16194_카드 구매하기 2(buying cards 2).py
@@ -2,14 +2,23 @@
 import sys
 
 n = int(sys.stdin.readline())
+
+p = [0] + list(map(int,sys.stdin.readline().split()))
 d = [-1] * (n + 1)
 d[0] = 0
-p = [0] * (n + 1)
-price = list(map(int,sys.stdin.readline().split()))
-for i, item in enumerate(price):
-    p[i+1] = item
-for i in range(1, n+1):
+d[1] = p[1]
+for i in range(2, n+1):
     for j in range(1, i+1):
         if d[i] == -1 or d[i] > d[i-j] + p[j]:
             d[i] = d[i - j] + p[j]
 print(d[n])
+
+"""
+d = [1000*10000] * (n+1)
+d[0] = 0
+d[1] = p[1]
+for i in range(2,n+1):
+    for j in range(1,i+1):
+        d[i] = min(d[i],p[j]+d[i-j])
+print(d[n])
+"""
