ccffbef6031f026db2739e8b52d630ca66d60af0
Beenie93 <seongbeen93@naver.com>
2020-05-04 02:52:07 +0900

    https://www.acmicpc.net/problem/11052

--- BOJ(Baekjoon Online Judge)/DP/11052_카드 구매하기(buying cards).py
+++ BOJ(Baekjoon Online Judge)/DP/11052_카드 구매하기(buying cards).py
@@ -2,13 +2,12 @@
 import sys
 
 n = int(sys.stdin.readline())
+p = [0] + list(map(int,sys.stdin.readline().split()))
 d = [0] * (n + 1)
-p = [0] * (n + 1)
-price = list(map(int,sys.stdin.readline().split()))
-for i, item in enumerate(price):
-    p[i+1] = item
-for i in range(1, n+1):
-    for j in range(1, i+1):
-        if d[i] < d[i-j] + p[j]:
-            d[i] = d[i-j] + p[j]
+
+d[1] = p[1]
+
+for i in range(2,n+1):
+    for j in range(1,i+1):
+        d[i] = max(d[i],p[j]+d[i-j])
 print(d[n])
