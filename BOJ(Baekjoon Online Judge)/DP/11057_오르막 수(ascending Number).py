4afe1a066c8942cd7888fe20a8dc0c2a3c5ffde6
Beenie93 <seongbeen93@naver.com>
2020-05-06 02:34:34 +0900

    https://www.acmicpc.net/problem/11057

---
+++ BOJ(Baekjoon Online Judge)/DP/11057_오르막 수(ascending Number).py
@@ -0,0 +1,30 @@
+#https://www.acmicpc.net/problem/11057
+
+import sys
+
+mod = 10007
+n = 1001
+d = [[0]*10 for _ in range(n)]
+
+for i in range(10):
+    d[1][i] = 1
+
+for i in range(2,n):
+    for j in range(10):
+        for k in range(j+1):
+            d[i][j] += d[i-1][k]
+        d[i][j] %= mod
+"""
+for i in range(2,n):
+    for j in range(10):
+        if j == 0:
+            d[i][j] = d[i-1][j]
+        else:
+            temp = j
+            while temp >= 0:
+                d[i][j] += d[i-1][temp]
+                temp -= 1
+        d[i][j] %= mod
+"""
+num = int(sys.stdin.readline())
+print(sum(d[num])%mod)
