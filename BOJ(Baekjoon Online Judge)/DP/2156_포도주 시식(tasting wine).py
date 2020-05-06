9c1c48a9b3a5d18d9af69a1417aeb070f9600fdb
Beenie93 <seongbeen93@naver.com>
2020-05-07 01:58:11 +0900

    https://www.acmicpc.net/problem/2156

---
+++ BOJ(Baekjoon Online Judge)/DP/2156_포도주 시식(tasting wine).py
@@ -0,0 +1,23 @@
+#https://www.acmicpc.net/problem/2156
+
+import sys
+
+n = int(sys.stdin.readline())
+a = [int(sys.stdin.readline()) for _ in range(n)]
+d = [0] * n
+d[0] = a[0]
+if n > 1:
+    d[1] = a[0] + a[1]
+
+    for i in range(2,n):
+        d[i] = max(d[i-1],d[i-2]+a[i],d[i-3]+a[i-1]+a[i])
+print(d[n-1])
+"""
+d = [[0] * 3 for _ in range(n)]
+d[0][1] = a[0]
+for i in range(1,n):
+    d[i][0] = max(d[i-1])
+    d[i][1] = d[i-1][0] + a[i]
+    d[i][2] = d[i-1][1] + a[i]
+print(max(d[n-1]))
+"""
