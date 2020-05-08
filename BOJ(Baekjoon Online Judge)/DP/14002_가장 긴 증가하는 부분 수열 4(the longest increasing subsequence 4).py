7fff62a24d6ab567f35e1de2de40f465415c8470
Beenie93 <seongbeen93@naver.com>
2020-05-09 01:58:02 +0900

    https://www.acmicpc.net/problem/14002

--- BOJ(Baekjoon Online Judge)/DP/14002_가장 긴 증가하는 부분 수열 4(the longest increasing subsequence 4).py
+++ BOJ(Baekjoon Online Judge)/DP/14002_가장 긴 증가하는 부분 수열 4(the longest increasing subsequence 4).py
@@ -1,6 +1,35 @@
 #https://www.acmicpc.net/problem/14002
 import sys
 
+n = int(sys.stdin.readline())
+a = list(map(int,sys.stdin.readline().split()))
+d = [0] * n
+v = [-1] * n
+d[0] = 1
+
+for i in range(1,n):
+    d[i] = 1
+    for j in range(i):
+        if a[i] > a[j] and d[i] == d[j]:
+            d[i] = d[j] + 1
+            v[i] = j
+index = 0
+temp = 0
+for i in range(n):
+    if temp < d[i]:
+        temp = d[i]
+        index = i
+ans = []
+while True:
+    ans.append(a[index])
+    if v[index] == -1:
+        break
+    index = v[index]
+
+print(len(ans))
+print(*sorted(ans))
+
+"""
 t = int(sys.stdin.readline())
 d = [0] * (t+1)
 v = [-1] * (t+1)
@@ -20,3 +49,5 @@ def go(p):
     go(v[p])
     print(a[p],end=' ')
 go(p)
+"""
+
