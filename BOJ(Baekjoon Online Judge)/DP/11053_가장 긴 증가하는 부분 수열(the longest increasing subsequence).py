7e96ebf276c8a0d9868e23e817a5eaa91d4fcd41
Beenie93 <seongbeen93@naver.com>
2020-05-08 23:54:45 +0900

    https://www.acmicpc.net/problem/11053

--- BOJ(Baekjoon Online Judge)/DP/11053_가장 긴 증가하는 부분 수열(the longest increasing subsequence).py
+++ BOJ(Baekjoon Online Judge)/DP/11053_가장 긴 증가하는 부분 수열(the longest increasing subsequence).py
@@ -1,12 +1,13 @@
 #https://www.acmicpc.net/problem/11053
 import sys
 
-t = int(sys.stdin.readline())
-d = [0] * (t+1)
-a = [0] + list(map(int,sys.stdin.readline().split()))
-for i in range(1,t+1):
+n = int(sys.stdin.readline())
+a = list(map(int,sys.stdin.readline().split()))
+d = [0] * n
+d[0] = 1
+for i in range(1,n):
     d[i] = 1
-    for j in range(1,i):
+    for j in range(i):
         if a[i] > a[j] and d[i] < d[j] + 1:
             d[i] = d[j] + 1
 print(max(d))
