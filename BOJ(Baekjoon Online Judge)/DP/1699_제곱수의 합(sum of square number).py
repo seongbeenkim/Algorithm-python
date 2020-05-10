c5d8747b6e3572c771a89f635ec4e38e36e3927b
Beenie93 <seongbeen93@naver.com>
2020-05-11 05:00:22 +0900

    https://www.acmicpc.net/problem/1699

--- BOJ(Baekjoon Online Judge)/DP/1699_제곱수의 합(sum of square number).py
+++ BOJ(Baekjoon Online Judge)/DP/1699_제곱수의 합(sum of square number).py
@@ -6,11 +6,9 @@ n = int(sys.stdin.readline())
 d = [0] *(n+1)
 d[1] = 1
 
-for i in range(1,n+1):
-    d[i] = i
-
 for i in range(2,n+1):
-    j = 2
+    d[i] = i
+    j = 1
     while j*j <= i:
         if d[i] > d[i-(j*j)] + 1:
             d[i] = d[i-(j*j)] + 1
