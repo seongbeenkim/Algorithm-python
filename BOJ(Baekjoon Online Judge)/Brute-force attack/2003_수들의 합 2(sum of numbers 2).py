29008feff7d4676967f7a3320f50f508b37e81ce
Beenie93 <seongbeen93@naver.com>
2020-05-28 16:20:51 +0900

    https://www.acmicpc.net/problem/2003

--- BOJ(Baekjoon Online Judge)/Brute-force attack/2003_수들의 합 2(sum of numbers 2).py
+++ BOJ(Baekjoon Online Judge)/Brute-force attack/2003_수들의 합 2(sum of numbers 2).py
@@ -8,7 +8,7 @@ left = 0
 right = 0
 sum = a[left]
 cnt = 0
-while (left <= right and right <= n-1):
+while (left <= right and right < n):
     if sum > m:
         sum -= a[left]
         left += 1
@@ -17,11 +17,35 @@ while (left <= right and right <= n-1):
             sum = a[left]
     elif sum < m:
         right += 1
-        if right <= n-1:
+        if right < n:
             sum += a[right]
     else:
         cnt += 1
         right += 1
-        if right <= n-1:
+        if right < n:
             sum += a[right]
 print(cnt)
+
+
+"""
+i = 0
+j = 0
+
+while j < n:
+    if i == j:
+        if a[i] == m:
+            ans += 1
+        j += 1
+
+    elif i < j:
+        total = sum(a[i:j+1])
+        if total == m:
+            ans += 1
+            j+=1
+        elif total > m:
+            i+=1
+        elif total < m:
+            j+=1
+
+print(ans)
+"""
