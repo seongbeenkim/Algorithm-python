0b8f51aaea9d5518fbdeb90e84ba8a6d6a1b3bb5
Beenie93 <seongbeen93@naver.com>
2020-06-04 13:39:23 +0900

    https://www.acmicpc.net/problem/11048

--- BOJ(Baekjoon Online Judge)/DP/11048_이동하기(Moving).py
+++ BOJ(Baekjoon Online Judge)/DP/11048_이동하기(Moving).py
@@ -1,7 +1,7 @@
 #https://www.acmicpc.net/problem/11048
 
 import sys
-
+from collections import deque
 n, m = map(int,sys.stdin.readline().split())
 a = [[0] * (m+1)] + [[0] + list(map(int,sys.stdin.readline().split())) for i in range(n)]
 d = [[0] * (m+1) for _ in range(n+1)]
@@ -31,4 +31,32 @@ for i in range(1,n+1):
         if j+1 <= m:
             d[i][j+1] = max(d[i][j+1],d[i][j] + a[i][j+1])
 print(d[n][m])
+"""
+
+#bfs
+"""
+n, m = map(int,sys.stdin.readline().split())
+a = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+d = [[-1] * m for _ in range(n)]
+check = [[False] * m for _ in range(n)]
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+ans = 0
+
+def bfs(i,j):
+    q = deque()
+    q.append((i,j))
+    d[i][j] = a[i][j]
+    while q:
+        x,y = q.popleft()
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < n and 0 <= ny < m and d[nx][ny] == -1:
+                d[nx][ny] = max(d[nx-1][ny],d[nx][ny-1]) + a[nx][ny]
+                q.append((nx,ny))
+
+
+bfs(0,0)
+print(max(ans,d[n-1][m-1]))
 """
