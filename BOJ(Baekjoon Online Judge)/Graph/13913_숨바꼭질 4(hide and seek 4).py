eac6bc404b0fd005e835758101e9a728c7a02b1e
Beenie93 <seongbeen93@naver.com>
2020-06-30 01:52:09 +0900

    https://www.acmicpc.net/problem/13913

--- BOJ(Baekjoon Online Judge)/Graph/13913_숨바꼭질 4(hide and seek 4).py
+++ BOJ(Baekjoon Online Judge)/Graph/13913_숨바꼭질 4(hide and seek 4).py
@@ -42,3 +42,38 @@ def go(n,k):
     print(k, end = " ")
 go(n,k)
 """
+
+
+"""
+n,k = map(int,sys.stdin.readline().split())
+d = [-1] * 200001
+v = [-1] * 200001
+
+q = deque()
+d[n] = 0
+v[n] = -1
+q.append(n)
+
+while q:
+    x = q.popleft()
+    for i in [1,-1,2]:
+        if i == 2:
+            dx = x * 2
+        else:
+            dx = x + i
+        if 0 <= dx <= 200000:
+            if d[dx] == -1 or d[dx] > d[x] + 1:
+                d[dx] = d[x] + 1
+                v[dx] = x
+                q.append(dx)
+
+print(d[k])
+ans = []
+ans.append(k)
+
+while v[k] != -1:
+    ans.append(v[k])
+    k = v[k]
+print(*ans[-1::-1])
+
+"""
