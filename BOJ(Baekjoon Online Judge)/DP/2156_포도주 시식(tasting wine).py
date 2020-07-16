09f9b476f82bdad629880a10e1aeb2d25929e782
Beenie93 <seongbeen93@naver.com>
2020-07-16 23:30:19 +0900

    https://www.acmicpc.net/problem/2156

--- BOJ(Baekjoon Online Judge)/DP/2156_포도주 시식(tasting wine).py
+++ BOJ(Baekjoon Online Judge)/DP/2156_포도주 시식(tasting wine).py
@@ -36,6 +36,26 @@ if n >= 2:
         d[i][0] = max(d[i-1])
         d[i][1] = d[i-1][0] + a[i]
         d[i][2] = d[i-1][1] + a[i]
+`
+print(max(d[n]))
+"""
+
+"""
+n = int(sys.stdin.readline())
+a = [0] + [int(sys.stdin.readline()) for _ in range(n)]
+d = [[0] * 4 for _ in range(n+1)]
+
+d[1][1] = a[1]
+
+if n >= 2:
+    d[2][1] = a[2]
+    d[2][2] = a[1] + a[2]
+    d[2][3] = d[1][1]
+
+    for i in range(3,n+1):
+        d[i][1] = d[i-1][3] + a[i]
+        d[i][2] = d[i-1][1] + a[i]
+        d[i][3] = max(d[i-1])
 
 print(max(d[n]))
 """
