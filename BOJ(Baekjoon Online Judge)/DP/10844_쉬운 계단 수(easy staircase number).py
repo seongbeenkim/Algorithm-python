6ed0a9fb54c676c60636b47de62f7f31856e7c60
Beenie93 <seongbeen93@naver.com>
2020-07-16 19:53:04 +0900

    https://www.acmicpc.net/problem/10844

--- BOJ(Baekjoon Online Judge)/DP/10844_쉬운 계단 수(easy staircase number).py
+++ BOJ(Baekjoon Online Judge)/DP/10844_쉬운 계단 수(easy staircase number).py
@@ -20,3 +20,21 @@ for i in range(2,max):
 n = int(sys.stdin.readline())
 print(sum(d[n])%mod)
 
+
+"""
+n = int(sys.stdin.readline())
+d = [[0] * 10 for _ in range(n+1)]
+mod = 1000000000
+for i in range(1,10):
+    d[1][i] = 1
+
+for i in range(2,n+1):
+    for j in range(10):
+        if j > 0:
+            d[i][j] += d[i-1][j-1]
+        d[i][j] %= mod
+        if j < 9:
+            d[i][j] += d[i-1][j+1]
+        d[i][j] %= mod
+print(sum(d[n])%mod)
+"""
