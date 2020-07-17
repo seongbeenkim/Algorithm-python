e44854be0e0018722588dd2631b81e53009a9d5e
Beenie93 <seongbeen93@naver.com>
2020-07-17 20:37:01 +0900

    https://www.acmicpc.net/problem/2225

--- BOJ(Baekjoon Online Judge)/DP/2225_합분해(sum decomposition).py
+++ BOJ(Baekjoon Online Judge)/DP/2225_합분해(sum decomposition).py
@@ -14,10 +14,25 @@ for i in range(1, k+1):
 
 """
 for i in range(1,k+1):
-    for j in rnage(n+1):
+    for j in range(n+1):
         d[i][j] = d[i-1][j] + d[i][j-1]
         d[i][j] %= mod
 """
 print(d[k][n])
 
 
+"""
+n, k = map(int,sys.stdin.readline().split())
+d = [[0] * (k+1) for _ in range(n+1)]
+d[0][0] = 1
+for i in range(1,n+1):
+    d[i][1] = 1
+
+for i in range(1,n+1):
+    for j in range(2,k+1):
+        for t in range(i+1):
+            d[i][j] += d[i-t][j-1]
+            d[i][j] %= 1000000000
+
+print(sum(d[n])%1000000000)
+"""
