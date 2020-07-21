014a8b626b9f88f520e2e57d3b63a22570fceaf4
Beenie93 <seongbeen93@naver.com>
2020-07-21 20:28:38 +0900

    https://www.acmicpc.net/problem/6064

--- BOJ(Baekjoon Online Judge)/Brute-force attack/6064_카잉 달력(cain calendar).py
+++ BOJ(Baekjoon Online Judge)/Brute-force attack/6064_카잉 달력(cain calendar).py
@@ -15,3 +15,43 @@ for i in range(t):
         k += m
     else:
         print(-1)
+
+
+"""
+t = int(sys.stdin.readline())
+
+def gcd(a,b):
+    if b == 0:
+        return a
+    return gcd(b,a%b)
+
+def lcm(a,b):
+    return a * b // gcd(a,b)
+
+for _ in range(t):
+    m,n,x,y = map(int,sys.stdin.readline().split())
+    i = 1
+    j = 1
+    cnt = 1
+    max = lcm(m,n)
+
+    while i != x:
+        i += 1
+        j += 1
+        cnt += 1
+        if j > n:
+            j = 1
+
+    while j != y:
+        if cnt > max:
+            break
+        j = (j+m)%n
+        if j == 0:
+            j = n
+        cnt += m
+
+    if cnt > max:
+        cnt = -1
+
+    print(cnt)
+"""
