ebb5c7aac20a5c6cd63ee5192f96488f7ebe54b9
Beenie93 <seongbeen93@naver.com>
2020-07-23 21:33:30 +0900

    https://www.acmicpc.net/problem/15654

--- BOJ(Baekjoon Online Judge)/Brute-force attack/15654_N과 M 5(N and M 5).py
+++ BOJ(Baekjoon Online Judge)/Brute-force attack/15654_N과 M 5(N and M 5).py
@@ -20,3 +20,24 @@ def go(index,n,m):
         go(index+1,n,m)
         c[i] = False
 go(0,n,m)
+
+
+"""
+sys.setrecursionlimit(10**6)
+
+
+n, m = map(int,sys.stdin.readline().split())
+a = list(map(int,sys.stdin.readline().split()))
+a.sort()
+
+def go(index,candi):
+    if len(candi) == m:
+        print(*candi)
+        return
+
+    for i in range(n):
+        if a[i] not in candi:
+            go(index+1,candi+[a[i]])
+
+go(0,[])
+"""
