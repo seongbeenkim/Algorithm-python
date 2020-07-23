db5ec232f4153738a734d47311c7b2458b2441b5
Beenie93 <seongbeen93@naver.com>
2020-07-23 22:49:35 +0900

    https://www.acmicpc.net/problem/15656

--- BOJ(Baekjoon Online Judge)/Brute-force attack/15656_N과 M 7(N and M 7).py
+++ BOJ(Baekjoon Online Judge)/Brute-force attack/15656_N과 M 7(N and M 7).py
@@ -15,3 +15,19 @@ def go(index):
         ans[index] = a[i]
         go(index+1)
 go(0)
+
+"""
+n, m = map(int,sys.stdin.readline().split())
+a = list(map(int,sys.stdin.readline().split()))
+a.sort()
+
+def go(index,candi):
+    if len(candi) == m:
+        print(*candi)
+        return
+
+    for i in range(n):
+        go(index+1,candi+[a[i]])
+
+go(0,[])
+"""
