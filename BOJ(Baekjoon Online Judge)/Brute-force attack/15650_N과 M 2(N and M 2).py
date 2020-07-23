a77ed967138f0859ae6f65029bcec9c1a188d6eb
Beenie93 <seongbeen93@naver.com>
2020-07-23 19:45:30 +0900

    https://www.acmicpc.net/problem/15650

--- BOJ(Baekjoon Online Judge)/Brute-force attack/15650_N과 M 2(N and M 2).py
+++ BOJ(Baekjoon Online Judge)/Brute-force attack/15650_N과 M 2(N and M 2).py
@@ -12,3 +12,27 @@ def go(index,n,m,l):
         ans[index] = i
         go(index+1,n,m,ans[index])
 go(0,n,m,0)
+
+"""
+n, m = map(int,sys.stdin.readline().split())
+
+def go(index,candi):
+
+    if len(candi) == m:
+        print(*candi)
+        return
+
+    for i in range(1,n+1):
+        if i not in candi:
+            is_bigger = True
+            for j in candi:
+                if i <= j:
+                    is_bigger = False
+                    break
+            if not is_bigger:
+                continue
+            go(index,candi+[i])
+
+for i in range(1,n+1):
+    go(i,[i])
+"""
