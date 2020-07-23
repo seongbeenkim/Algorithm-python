8883a20e6fccfe8f30919a7b8d201d81a0d1e3cf
Beenie93 <seongbeen93@naver.com>
2020-07-23 22:48:07 +0900

    https://www.acmicpc.net/problem/15655

--- BOJ(Baekjoon Online Judge)/Brute-force attack/15655_N과 M 6(N and M 6).py
+++ BOJ(Baekjoon Online Judge)/Brute-force attack/15655_N과 M 6(N and M 6).py
@@ -30,3 +30,22 @@ def go(index,seq):
 go(0,[])
 for i in ans:
     print(*i)
+
+"""
+n, m = map(int,sys.stdin.readline().split())
+a = list(map(int,sys.stdin.readline().split()))
+a.sort()
+
+def go(index,candi):
+    if len(candi) == m:
+        print(*candi)
+        return
+
+    for i in range(n):
+        if len(candi) == 0:
+            go(index + 1, candi + [a[i]])
+        elif a[i] not in candi and candi[-1] < a[i]:
+            go(index+1,candi+[a[i]])
+
+go(0,[])
+"""
