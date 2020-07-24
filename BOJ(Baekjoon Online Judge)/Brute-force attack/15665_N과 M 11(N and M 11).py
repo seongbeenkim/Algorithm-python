b5c120073ea1b0565f44ac9a6c8da01bc0eac815
Beenie93 <seongbeen93@naver.com>
2020-07-24 19:39:08 +0900

    https://www.acmicpc.net/problem/15665

--- BOJ(Baekjoon Online Judge)/Brute-force attack/15665_N과 M 11(N and M 11).py
+++ BOJ(Baekjoon Online Judge)/Brute-force attack/15665_N과 M 11(N and M 11).py
@@ -31,3 +31,27 @@ ans = sorted(list(set(ans)))
 for v in ans:
     print(*v)
 """
+
+"""
+n, m = map(int,sys.stdin.readline().split())
+a = list(map(int,sys.stdin.readline().split()))
+a.sort()
+
+candi = []
+def go(index,ans):
+    global candi
+    if len(ans) == m:
+        candi.append(tuple(ans))
+        return
+
+    if index >= n:
+        return
+
+    for i in range(n):
+        go(index+1,ans + [a[i]])
+go(0,[])
+ans = list(set(candi))
+ans.sort()
+for i in ans:
+    print(*i)
+"""
