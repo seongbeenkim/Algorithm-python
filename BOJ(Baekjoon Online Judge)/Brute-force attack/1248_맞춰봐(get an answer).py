196968dade91ceff60207985be6a6c9a63043fe1
Beenie93 <seongbeen93@naver.com>
2020-07-29 15:02:10 +0900

    https://www.acmicpc.net/problem/1248

--- BOJ(Baekjoon Online Judge)/Brute-force attack/1248_맞춰봐(get an answer).py
+++ BOJ(Baekjoon Online Judge)/Brute-force attack/1248_맞춰봐(get an answer).py
@@ -3,50 +3,53 @@
 import sys
 
 n = int(sys.stdin.readline())
-op = sys.stdin.readline()
-s = [[0] * n for i in range(n)]
+s = sys.stdin.readline().rstrip()
+op = [[0] * n for _ in range(n)]
 ans = [0] * n
+
 cnt = 0
 for i in range(n):
     for j in range(i,n):
-        if op[cnt] == "0":
-            s[i][j] = 0
-        elif op[cnt] == "-":
-            s[i][j] = -1
+        if s[cnt] == "0":
+            op[i][j] = 0
+        elif s[cnt] == "+":
+            op[i][j] = 1
         else:
-            s[i][j] = 1
-        cnt += 1
+            op[i][j] = -1
+        cnt+=1
 
-def ok():
-    for i in range(n):
-        sum = 0
-        for j in range(i,n):
-            sum += ans[j]
-            if s[i][j] == 0:
-                if sum != 0:
-                    return False
-            elif s[i][j] < 0:
-                if sum >= 0:
-                    return False
-            elif s[i][j] > 0:
-                if sum <= 0:
-                    return False
+def check(index):
+    sum = 0
+    for i in range(index,-1,-1):
+        sum += ans[i]
+
+        if op[i][index] == 0 and sum != 0:
+            return False
+        elif op[i][index] == 1 and sum <= 0 :
+            return False
+        elif op[i][index] == -1 and sum >= 0:
+            return False
+
     return True
 
 def go(index):
     if index == n:
-        return ok()
+        return True
 
-    if s[index][index] == 0:
+    if op[index][index] == 0:
         ans[index] = 0
-        return go(index+1)
+        if check(index):
+            if go(index+1):
+                return True
+        return False
 
     for i in range(1,11):
-        ans[index] = i * s[index][index]
-        if (go(index+1)):
-            return True
+        ans[index] = i * op[index][index]
+        if check(index):
+            if go(index+1):
+                return True
 
     return False
 
 go(0)
-print(' '.join(map(str,ans)))
+print(*ans)
