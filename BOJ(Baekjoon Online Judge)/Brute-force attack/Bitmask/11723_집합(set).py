f91e7e80a8070acf625597f18a408513bdb841cf
Beenie93 <seongbeen93@naver.com>
2020-07-30 14:43:30 +0900

    https://www.acmicpc.net/problem/11723

--- BOJ(Baekjoon Online Judge)/Brute-force attack/Bitmask/11723_집합(set).py
+++ BOJ(Baekjoon Online Judge)/Brute-force attack/Bitmask/11723_집합(set).py
@@ -24,3 +24,44 @@ for i in range(n):
         s = (1 << 20) - 1
     else:
         s = 0
+
+"""
+class sets():
+    def __init__(self):
+        self.s = 0
+    def add(self,x):
+        self.s = (self.s | (1<<x))
+    def remove(self,x):
+        self.s = (self.s & ~(1<<x))
+    def check(self,x):
+        if self.s & (1<<x):
+            print(1)
+        else:
+            print(0)
+    def toggle(self,x):
+        self.s = (self.s ^ (1<<x))
+    def all(self):
+        self.s = (1<<21)-1
+    def empty(self):
+        self.s = 0
+
+n = int(sys.stdin.readline())
+s = sets()
+for i in range(n):
+    op, *num = sys.stdin.readline().split()
+    if len(num) > 0:
+        x = int(num[0])
+    if op == "add":
+        s.add(x)
+    elif op == "remove":
+        s.remove(x)
+    elif op == "check":
+        s.check(x)
+    elif op == "toggle":
+        s.toggle(x)
+    elif op == "all":
+        s.all()
+    else:
+        s.empty()
+
+"""
