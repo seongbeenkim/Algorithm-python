fabd289e6e1970c73b2b375fd7ac2122ab9b6e10
Beenie93 <seongbeen93@naver.com>
2020-08-01 11:06:56 +0900

    https://www.acmicpc.net/problem/10815

--- BOJ(Baekjoon Online Judge)/Divide & Conquer/10815_숫자 카드(number card).py
+++ BOJ(Baekjoon Online Judge)/Divide & Conquer/10815_숫자 카드(number card).py
@@ -28,3 +28,29 @@ for x in numbers:
     go(x)
 for x in ans:
     print(x , end = " ")
+
+"""
+n = int(sys.stdin.readline())
+a = list(map(int,sys.stdin.readline().split()))
+a.sort()
+m = int(sys.stdin.readline())
+num = list(map(int,sys.stdin.readline().split()))
+
+def go(i,j,x):
+    ans = 0
+    mid = (i+j)//2
+    if i > j:
+        return 0
+
+    if a[mid] == x:
+        return 1
+    elif a[mid] > x:
+        ans += go(i,mid-1,x)
+    else:
+        ans += go(mid+1,j,x)
+
+    return ans
+
+for i in range(m):
+    print(go(0,n-1,num[i]), end = " ")
+"""
