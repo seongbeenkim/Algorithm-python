34183a6fac948a07d94a905cce151d33475d70c9
Beenie93 <seongbeen93@naver.com>
2020-08-09 14:29:14 +0900

    https://www.acmicpc.net/problem/1261

--- BOJ(Baekjoon Online Judge)/Graph/1261_알고스팟(algospot).py
+++ BOJ(Baekjoon Online Judge)/Graph/1261_알고스팟(algospot).py
@@ -29,3 +29,14 @@ def bfs(start,end):
                     d[nx][ny] = d[x][y]
 bfs(0,0)
 print(d[n-1][m-1])
+
+# 재탐색을 허용하기 때문에 시간이 조금 더 오래걸린다.
+"""
+            if 0 <= nx < n and 0 <= ny < m:
+                if (d[nx][ny] == -1 or d[x][y] < d[nx][ny]) and a[nx][ny] == '0':
+                    d[nx][ny] = d[x][y]
+                    q.append((nx,ny))
+                elif (d[nx][ny] == -1 or d[x][y] + 1 < d[nx][ny]) and a[nx][ny] == '1':
+                    d[nx][ny] = d[x][y] + 1
+                    q.append((nx,ny))
+"""
