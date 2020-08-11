4613cd42865898f58b0b091a5eca8cced82771b0
Beenie93 <seongbeen93@naver.com>
2020-08-11 12:35:38 +0900

    https://www.acmicpc.net/problem/2138

--- BOJ(Baekjoon Online Judge)/Greedy/2138_전구와 스위치(light bulb and switch).py
+++ BOJ(Baekjoon Online Judge)/Greedy/2138_전구와 스위치(light bulb and switch).py
@@ -46,3 +46,43 @@ elif result == a:
     print(unclicked_cnt)
 elif result != clicked and result != a:
     print(-1)
+
+"""
+n = int(sys.stdin.readline())
+a = list(map(int,sys.stdin.readline().rstrip()))
+b = list(map(int,sys.stdin.readline().rstrip()))
+
+copy_a = a.copy()
+copy_a[0] = 1 - copy_a[0]
+copy_a[1] = 1 - copy_a[1]
+
+def count(a):
+    cnt = 0
+    for i in range(1,n):
+        if a[i-1] != b[i-1]:
+            if i == n-1:
+                a[n-1] = 1 - a[n-1]
+                a[n-2] = 1 - a[n-2]
+            else:
+                a[i-1] = 1 - a[i-1]
+                a[i] = 1 - a[i]
+                a[i+1] = 1 - a[i+1]
+            cnt += 1
+
+    for i in range(n):
+        if a[i] != b[i]:
+            cnt = -1
+    return cnt
+
+res = count(a)
+res_2 = count(copy_a)
+ans = -1
+if res != -1 and res_2 != -1:
+    ans = min(res,res_2+1)
+elif res == -1 and res_2 != -1:
+    ans = res_2+1
+elif res_2 == -1 and res != -1:
+    ans = res
+print(ans)
+
+"""
