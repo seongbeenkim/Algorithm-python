2ad175818be5f9b58153bfdd441adf9c7b149705
Beenie93 <seongbeen93@naver.com>
2020-08-11 20:59:57 +0900

    https://www.acmicpc.net/problem/1285

--- BOJ(Baekjoon Online Judge)/Greedy/1285_동전 뒤집기(flipping coins).py
+++ BOJ(Baekjoon Online Judge)/Greedy/1285_동전 뒤집기(flipping coins).py
@@ -5,11 +5,11 @@ import sys
 n = int(sys.stdin.readline())
 a = [list(sys.stdin.readline().rstrip()) for _ in range(n)]
 ans = 401
-for k in range(1<<n):
+for k in range(1<<n): # 모든 행을 뒤집는 것에 대한 모든 경우의 수 = 2^n
     sum = 0
-    for j in range(n):
+    for j in range(n): # 열
         cnt = 0
-        for i in range(n):
+        for i in range(n): # 행
             c = a[i][j]
             if (1<<i) & k:
                 if c == "T":
