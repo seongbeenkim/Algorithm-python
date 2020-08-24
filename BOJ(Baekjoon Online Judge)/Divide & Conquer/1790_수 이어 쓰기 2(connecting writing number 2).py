95d9a988459cf7520f04208562a2901069a8711e
Beenie93 <seongbeen93@naver.com>
2020-08-24 16:28:44 +0900

    https://www.acmicpc.net/problem/1790

--- BOJ(Baekjoon Online Judge)/Divide & Conquer/1790_수 이어 쓰기 2(connecting writing number 2).py
+++ BOJ(Baekjoon Online Judge)/Divide & Conquer/1790_수 이어 쓰기 2(connecting writing number 2).py
@@ -34,4 +34,51 @@ else:
             right = mid - 1
     s = str(ans)
     l = cal(ans)
-    print(s[len(s) -1 - (l-k)] )
+    print(s[len(s) -1 - (l-k)])
+
+
+"""
+n, k = map(int,sys.stdin.readline().split())
+
+ans = -1
+
+def check(n,k):
+    cur = 0
+    limit = 0
+    while n >= 10 ** cur:
+        cur += 1
+        limit += cur * 9 * (10**(cur-1))
+
+    limit -= cur * 9 * (10**(cur-1))
+
+    difference = n - (10**(cur-1)) + 1
+    limit += difference * cur
+
+    return True if limit >= k else False
+
+if check(n,k):
+
+    cur = 0
+    number = 0
+
+    while k > number:
+        cur += 1
+        number += cur * 9 * (10 ** (cur - 1))
+    number -= cur * 9 * (10 ** (cur - 1))
+
+    count = k - number
+
+    start = 10 ** (cur - 1)
+
+    difference = count // cur
+    remain = count % cur
+
+    start += difference
+
+    if remain == 0:
+        ans = list(str(start - 1))[-1]
+    else:
+        ans = list(str(start))[remain - 1]
+
+print(ans)
+"""
