bff5e3e233611a49aea33d541005b9b11caa8485
Beenie93 <seongbeen93@naver.com>
2020-08-31 22:33:27 +0900

    https://www.acmicpc.net/problem/1786

--- BOJ(Baekjoon Online Judge)/String/1786_찾기(searching).py
+++ BOJ(Baekjoon Online Judge)/String/1786_찾기(searching).py
@@ -41,3 +41,47 @@ ans = kmp()
 print(len(ans))
 for i in ans:
     print(i+1)
+
+"""
+t = list(sys.stdin.readline().rstrip())
+p = list(sys.stdin.readline().rstrip())
+
+pi = [0] * len(p)
+t_pi = [0] * len(t)
+
+for i in range(1,len(p)):
+    j=i
+
+    while pi[j-1] != 0 and p[i] != p[pi[j-1]]:
+        j = pi[j-1]
+    if pi[j-1] == 0:
+        if p[i] == p[0]:
+            pi[i] = 1
+
+    if p[i] == p[pi[j-1]]:
+        pi[i] = pi[j-1] + 1
+
+j = 0
+for i in range(len(t)):
+
+    while j > 0 and t[i] != p[j]:
+        j = pi[j-1]
+
+    if j == 0:
+        if t[i] == p[j]:
+            t_pi[i] = 1
+            j += 1
+    else:
+        if t[i] == p[j]:
+            t_pi[i] = j + 1
+            j += 1
+            if j == len(p):
+                j = 0
+ans = []
+for i in range(len(t_pi)):
+    if t_pi[i] == len(p):
+        ans.append((i+1)-len(p)+1)
+print(len(ans))
+for i in ans:
+    print(i)
+"""
