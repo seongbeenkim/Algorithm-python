c50a955cd5f995911ca6660d6f144dce49b5d83c
Beenie93 <seongbeen93@naver.com>
2020-09-07 20:27:16 +0900

    #https://www.acmicpc.net/problem/1761

--- BOJ(Baekjoon Online Judge)/Graph/1761_정점들의 거리(vertex distance).py
+++ BOJ(Baekjoon Online Judge)/Graph/1761_정점들의 거리(vertex distance).py
@@ -47,3 +47,58 @@ m = int(sys.stdin.readline())
 for _ in range(m):
     u, v = map(int,sys.stdin.readline().split())
     print(dist[v] + dist[u] - (2*dist[lca(u,v)]))
+
+
+"""
+n = int(sys.stdin.readline())
+
+parent = [0] * (n+1)
+d = [0] * (n+1)
+dist = [0] * (n+1)
+a = [[] for _ in range(n+1)]
+check = [False] * (n+1)
+
+for _ in range(n-1):
+    s, e, k = map(int,sys.stdin.readline().split())
+    a[s].append((e,k))
+    a[e].append((s,k))
+
+q = deque()
+q.append((1))
+check[1] = True
+
+while q:
+    x = q.popleft()
+    for y, k in a[x]:
+        if not check[y]:
+            check[y] = True
+            d[y] = d[x] + 1
+            dist[y] = k
+            parent[y] = x
+            q.append(y)
+
+def lca(x,y):
+    res_x = 0
+    res_y = 0
+
+    if d[x] < d[y]:
+        x,y = y,x
+
+    while d[x] != d[y]:
+        res_x += dist[x]
+        x = parent[x]
+
+    while x != y:
+        res_x += dist[x]
+        res_y += dist[y]
+        x = parent[x]
+        y = parent[y]
+
+    return res_x + res_y
+
+m = int(sys.stdin.readline())
+print(d,dist,parent)
+for _ in range(m):
+    s, e = map(int,sys.stdin.readline().split())
+    print(lca(s,e))
+"""
