914cf9fd92f87c864fbb7eb8ad626c512f069cae
Beenie93 <seongbeen93@naver.com>
2020-09-08 19:05:47 +0900

    https://programmers.co.kr/learn/courses/30/lessons/2252

--- BOJ(Baekjoon Online Judge)/Graph/2252_줄 세우기(line up).py
+++ BOJ(Baekjoon Online Judge)/Graph/2252_줄 세우기(line up).py
@@ -3,6 +3,8 @@
 import sys
 from collections import deque
 
+sys.setrecursionlimit(10**5)
+
 n, m = map(int,sys.stdin.readline().split())
 a = [[] for _ in range(n+1)]
 val = [0] * (n+1)
@@ -13,19 +15,16 @@ s = []
 for _ in range(m):
     x,y = map(int,sys.stdin.readline().split())
     a[y].append(x)
-    val[y] += 1
 
 def dfs(x):
     check[x] = True
     for i in a[x]:
         if check[i] == False:
-            s.append(i)
             dfs(i)
-
+    s.append(x)
 
 for i in range(1,n+1):
     if check[i] == False:
-        s.append(i)
         dfs(i)
 print(" ".join(map(str,s)))
 
