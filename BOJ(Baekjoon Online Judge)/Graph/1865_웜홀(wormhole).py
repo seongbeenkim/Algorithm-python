2c7d88e0b2a0a6b2884fb76652f56958f1143124
Beenie93 <seongbeen93@naver.com>
2020-09-10 00:16:16 +0900

    https://www.acmicpc.net/problem/1865

--- BOJ(Baekjoon Online Judge)/Graph/1865_웜홀(wormhole).py
+++ BOJ(Baekjoon Online Judge)/Graph/1865_웜홀(wormhole).py
@@ -6,6 +6,8 @@ tc = int(sys.stdin.readline())
 for _ in range(tc):
     n,m,w = map(int,sys.stdin.readline().split())
     path = []
+    maximum = 2147383647
+    dist = [maximum] * (n + 1) # 아무 값이나 넣어줘도 상관없다.
     for _ in range(m):
         s,e,t = map(int,sys.stdin.readline().split())
         path.append([s,e,t])
@@ -13,11 +15,11 @@ for _ in range(tc):
     for _ in range(w):
         s, e, t = map(int, sys.stdin.readline().split())
         path.append([s,e,-t])
-    dist = [0] * (n+1)
+    dist[1] = 0 # 넣어줄 필요가 없다.
     negative = False
     for i in range(1,n+1):
         for s, e, t in path:
-            if dist[e] > dist[s] + t:
+            if dist[e] > dist[s] + t: # dist[s] != maximum을 해줄 필요가 없다. 모든 정점이 연결되어 있다고 할 수도 없어서 해당 정점이 maximum이더라도 검사를 해주어 그 곳에서 시작되는 음의 사이클이 있는지만 찾기만 하면 되기 때문이다.
                 dist[e] = dist[s] + t
                 if i == n:
                     negative = True
