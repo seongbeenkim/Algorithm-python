01f059ae33e67e350603d12cb8ad81319a5f2aec
Beenie93 <seongbeen93@naver.com>
2020-09-10 15:06:40 +0900

    https://www.acmicpc.net/problem/1916

--- BOJ(Baekjoon Online Judge)/Graph/1916_최소비용 구하기(minimum cost).py
+++ BOJ(Baekjoon Online Judge)/Graph/1916_최소비용 구하기(minimum cost).py
@@ -28,3 +28,30 @@ while q:
                 heapq.heappush(q,[dist[e],e])
                 check[s] = True
 print(dist[end])
+
+"""
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+
+a = [[] for _ in range(n+1)]
+d = [214738364] * (n+1)
+
+for _ in range(m):
+    x, y, c = map(int,sys.stdin.readline().split())
+    a[x].append((c,y))
+
+start, end = map(int,sys.stdin.readline().split())
+
+h = []
+d[start] = 0
+heapq.heappush(h,(0,start))
+
+while h:
+    c, x = heapq.heappop(h)
+    for cost, y in a[x]:
+        if d[y] > d[x] + cost:
+            d[y] = d[x] + cost
+            heapq.heappush(h,(d[y],y))
+
+print(d[end])
+"""
