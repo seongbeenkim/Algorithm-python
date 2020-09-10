f9b04da678b3cd354c06a6b600a176e97f7e37ec
Beenie93 <seongbeen93@naver.com>
2020-09-10 16:12:15 +0900

    https://www.acmicpc.net/problem/11403

--- BOJ(Baekjoon Online Judge)/Graph/11403_경로 찾기(finding path).py
+++ BOJ(Baekjoon Online Judge)/Graph/11403_경로 찾기(finding path).py
@@ -9,7 +9,7 @@ d = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
 for k in range(n):
     for i in range(n):
         for j in range(n):
-            if d[i][k] != 0 and d[k][j] != 0:
+            if d[i][k] != 0 and d[k][j] != 0: #if d[i][k] + d[k][j] == 2:
                 d[i][j] = 1
 for i in range(n):
     print(*d[i])
