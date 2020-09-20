500a128b69c7fd35a5b72d37c2c6bf598e068664
Beenie93 <seongbeen93@naver.com>
2020-09-21 00:07:28 +0900

    https://www.acmicpc.net/problem/1987

--- BOJ(Baekjoon Online Judge)/Brute-force attack/1987_알파벳(alphabet).py
+++ BOJ(Baekjoon Online Judge)/Brute-force attack/1987_알파벳(alphabet).py
@@ -5,22 +5,27 @@ import sys
 n, m = map(int,sys.stdin.readline().split())
 a = [list(sys.stdin.readline()) for _ in range(n)]
 check = [False] * 26
-ans = 0
+
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
 def go(i,j,cnt):
-    global ans
-    ans = max(cnt,ans)
+    ans = 1
+
+    if ans < cnt:
+        ans = cnt
 
     for k in range(4):
         nx = i + dx[k]
         ny = j + dy[k]
-        if 0 <= nx < n and 0 <= ny < m and not check[ord(a[nx][ny])-ord('A')]:
-            check[ord(a[nx][ny]) - ord('A')] = True
-            go(nx,ny,cnt+1)
-            check[ord(a[nx][ny]) - ord('A')] = False
+        if 0 <= nx < n and 0 <= ny < m:
+            alpha = ord(a[nx][ny])-ord('A')
+            if not check[alpha]:
+                check[alpha] = True
+                temp = go(nx,ny,cnt+1)
+                ans = max(temp,ans)
+                check[alpha] = False
+    return ans
 
 check[ord(a[0][0]) - ord('A')] = True
-go(0,0,1)
-print(ans)
+print(go(0,0,1))
