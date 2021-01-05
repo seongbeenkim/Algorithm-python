01ca682704c388bdcc451a4ef7f95f6deb79ea55
Beenie93 <seongbeen93@naver.com>
2021-01-05 19:04:20 +0900

    https://programmers.co.kr/learn/courses/30/lessons/70128

---
+++ Programmers/Level1/내적(dot product).py
@@ -0,0 +1,7 @@
+# https://programmers.co.kr/learn/courses/30/lessons/70128
+
+def solution(a, b):
+    return sum(x * y for x, y in zip(a, b))
+
+print(solution([1, 2, 3, 4], [-3, -1, 0, 2]))
+print(solution([-1, 0, 1], [1, 0, -1]))
