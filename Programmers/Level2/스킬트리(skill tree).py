45d8ca69f16a41577d06db38abe4f21b578829a6
Beenie93 <seongbeen93@naver.com>
2021-01-15 23:58:57 +0900

    https://programmers.co.kr/learn/courses/30/lessons/49993#fnref1

--- Programmers/Level2/스킬트리(skill tree).py
+++ Programmers/Level2/스킬트리(skill tree).py
@@ -30,4 +30,24 @@ def solution(skill, skill_trees):
     return answer
 
 
+#
+# from collections import deque
+#
+# def solution(skill, skill_trees):
+#     answer = 0
+#
+#     for skills in skill_trees:
+#         skill_list = deque(skill)
+#         is_wrong = False
+#
+#         for s in skills:
+#             if s in skill:
+#                 if s != skill_list.popleft():
+#                     is_wrong = True
+#                     break
+#         if not is_wrong:
+#             answer += 1
+#
+#     return answer
+
 print(solution("CBD", ["BACDE", "CBADF", "AECB", "BDA"]))
