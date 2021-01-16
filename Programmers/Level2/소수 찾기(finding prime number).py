b17515f841a12fa34c5d73f06f6ae5a410df21e4
Beenie93 <seongbeen93@naver.com>
2021-01-16 23:57:00 +0900

    https://programmers.co.kr/learn/courses/30/lessons/42839

---
+++ Programmers/Level2/소수 찾기(finding prime number).py
@@ -0,0 +1,16 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42839
+
+from itertools import permutations
+
+def solution(n):
+    a = set()
+    for i in range(len(n)):
+        a |= set(map(int, map("".join, permutations(list(n), i + 1))))
+    a -= set(range(0, 2))
+    for i in range(2, int(max(a) ** 0.5) + 1):
+        a -= set(range(i * 2, max(a) + 1, i))
+    return len(a)
+
+
+print(solution("17"))
+print(solution("011"))
