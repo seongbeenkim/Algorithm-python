05e0d3f0270557c9e8bf76c587cb4865a6b07fa2
Beenie93 <seongbeen93@naver.com>
2021-01-17 23:00:31 +0900

    https://programmers.co.kr/learn/courses/30/lessons/49993#fnref1

--- Programmers/Level2/소수 찾기(finding prime number).py
+++ Programmers/Level2/소수 찾기(finding prime number).py
@@ -2,6 +2,7 @@
 
 from itertools import permutations
 
+
 def solution(n):
     a = set()
     for i in range(len(n)):
@@ -11,6 +12,49 @@ def solution(n):
         a -= set(range(i * 2, max(a) + 1, i))
     return len(a)
 
+
+#
+# def solution(numbers):
+#     n = len(numbers)
+#
+#     def go(num, m):
+#         result = []
+#
+#         if len(num) == m:
+#             return [int(num)]
+#
+#         for i in range(n):
+#             if check[i]:
+#                 continue
+#             check[i] = True
+#             result += go(num + numbers[i], m)
+#             check[i] = False
+#
+#         return result
+#
+#     temp = []
+#
+#     for i in range(n):
+#         check = [False] * n
+#         temp += go('', i + 1)
+#
+#     temp = list(set(temp))
+#     maximum = max(temp) + 1
+#     check = [False] * maximum
+#     answer = 0
+#
+#     for i in range(2, maximum):
+#         if check[i] == False and i in temp:
+#             check[i] = True
+#             answer += 1
+#
+#         k = i * i
+#
+#         while k < maximum:
+#             check[k] = True
+#             k += i
+#
+#     return answer
 
 print(solution("17"))
 print(solution("011"))
