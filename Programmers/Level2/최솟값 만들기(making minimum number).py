64c215a16929d852ae63296220b8504aaf2a3bf6
Beenie93 <seongbeen93@naver.com>
2021-01-28 23:54:00 +0900

    https://programmers.co.kr/learn/courses/30/lessons/12941

---
+++ Programmers/Level2/최솟값 만들기(making minimum number).py
@@ -0,0 +1,14 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12941
+
+def solution(A, B):
+    answer = 0
+    A.sort()
+    B.sort(reverse=True)
+
+    for i in range(len(A)):
+        answer += A[i] * B[i]
+    return answer
+
+
+print(solution([1, 4, 2], [5, 4, 4]))
+print(solution([1, 2], [3, 4]))
